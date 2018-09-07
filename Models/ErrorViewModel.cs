d668862199a559a7e7b35bdf68175dc4f3ded199
lam tuyen <tuyenlqvnp@gmail.com>
2018-09-07 11:27:24 +0700

    begin

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace vnpt_einvoice.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
